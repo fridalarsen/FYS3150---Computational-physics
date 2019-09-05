e0fb8155f7d7b9ac6af81b379ae4d20644352a0d
Frida Larsen <friidamor@gmail.com>
2019-09-05 10:55:24 +0200

    Added code for timing algorithm.

--- Project1/special_tridiag_matrix_algorithm.py
+++ Project1/special_tridiag_matrix_algorithm.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
-def special_tma(f, n):
+def special_tma(f, x):
     """
     Tridiagonal matrix algorithm for the special case where the upper and lower
     diagonal are the same, and the arrays only consist of the same value.
@@ -14,48 +15,39 @@ def special_tma(f, n):
         Solution u of the problem Au = f in the specialized case.
 
     """
-    flops = 0
-
+    n       = len(x)
     u       = np.zeros(n)
     f_tilde = np.zeros(n)
-    b_tilde = np.array([(i+1)/i for i in range(1,n+1)])
-    flops  += 2*n
-
     h = x[1] - x[0]
     b_squig = h**2*f
-    flops  += 1
-    flops  += 2*(n+2)
+
+    start = time.process_time()
+    b_tilde = np.array([(i+1)/i for i in range(1,n+1)])
 
     f_tilde[0] = b_squig[0]
     for i in range(1,n):
         i_ = i/(i+1)
-        flops += 2
         f_tilde[i] = b_squig[i] + i_*f_tilde[i-1]
-        flops += 2
-
 
     u[n-1] = f_tilde[n-1]/b_tilde[n-1]
-    flops += 1
     for i in range(n-2, -1, -1):
         i_ = (i+1)/(i+2)
-        flops += 3
-        u[i] = i_*(f_tilde[i]+u[i+1])
-        flops += 2
 
-    return np.concatenate([np.zeros(1), u, np.zeros(1)]), flops
+        u[i] = i_*(f_tilde[i]+u[i+1])
+    end = time.process_time()
+    runtime = end - start
+
+    return np.concatenate([np.zeros(1), u, np.zeros(1)]), runtime
 
 if __name__ == '__main__':
     n_ = [10, 100, 1000]
 
     for i in n_:
-
         x = np.linspace(0, 1, i+2)
         f = 100*np.exp(-10*x)
         sol  = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
 
-        alg_2, flops = special_tma(f[1:-1], i)
-
-        print("Number of floating point operations for n={}: {}".format(i, flops))
+        alg_2, runtime = special_tma(f[1:-1], x[1:-1])
 
         plt.plot(x, sol, label="Closed-form solution")
         plt.plot(x, alg_2, label="Specialized algorithm solution")
