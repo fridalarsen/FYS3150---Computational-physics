0ef4fdfaa0433260f181299296e891a13172a0d1
Frida Larsen <friidamor@gmail.com>
2019-09-05 10:36:47 +0200

    Added code for timing algorithm.

--- Project1/tridiag_matrix_algorithm.py
+++ Project1/tridiag_matrix_algorithm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 
 def tridiagonal_matrix_algorithm(a, b, c, f, x):
@@ -24,6 +25,7 @@ def tridiagonal_matrix_algorithm(a, b, c, f, x):
     c_prime = np.zeros(n-1)
     d_prime = np.zeros(n)
 
+    start = time.process_time()
     c_prime[0]  = c[0] / b[0]
     d_prime[0]  = b_squig[0] / b[0]
 
@@ -38,7 +40,10 @@ def tridiagonal_matrix_algorithm(a, b, c, f, x):
     for k in range(n-2, -1, -1):
         u[k] = d_prime[k] - (c_prime[k]*u[k+1])
 
-    return np.concatenate([np.zeros(1), u, np.zeros(1)])
+    end = time.process_time()
+    runtime = end - start
+
+    return np.concatenate([np.zeros(1), u, np.zeros(1)]), runtime
 
 def run_tma(n):
     """
@@ -60,10 +65,10 @@ def run_tma(n):
     b = np.ones(n)*(2)
     c = np.ones(n-1)*(-1)
 
-    u_algorithm = tridiagonal_matrix_algorithm(a, b, c, f[1:-1], x[1:-1])
+    u_algorithm, runtime = tridiagonal_matrix_algorithm(a, b, c, f[1:-1], x[1:-1])
     u_solution  = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
 
-    return x, u_solution, u_algorithm
+    return x, u_solution, u_algorithm, runtime
 
 
 if __name__ == '__main__':
@@ -71,7 +76,7 @@ if __name__ == '__main__':
     n_ = [10, 100, 1000]
 
     for i in n_:
-        x, sol, algorithm = run_tma(i)
+        x, sol, algorithm, runtime = run_tma(i)
         plt.plot(x, sol, label="Closed-form solution")
         plt.plot(x, algorithm, label="Algorithm solution")
         plt.legend()
