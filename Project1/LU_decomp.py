2fa8de27f9e992a1634ddf47e41df961f2eb449e
Frida Larsen <friidamor@gmail.com>
2019-09-06 13:01:42 +0200

    Added time calculations and errorbar plotting

--- Project1/LU_decomp.py
+++ Project1/LU_decomp.py
@@ -4,30 +4,36 @@ import matplotlib.pyplot as plt
 import time
 
 
-n = [int(m) for m in [1e1, 1e2, 1e3]]
-t = np.zeros(len(n))
+n = [int(m) for m in [1e1, 1e2, 1e3, 1e4]]
+m = 5
 
+times_LU_decomp = np.zeros([len(n),m])
 for i in range(len(n)):
-    a = np.ones(n[i]-1) * (-1)
-    b = np.ones(n[i]) * (2)
-    A = np.diag(b)
+    t = np.zeros(m)
+    for j in range(m):
+        a = np.ones(n[i]-1) * (-1)
+        b = np.ones(n[i]) * (2)
+        A = np.diag(b)
 
-    for j in range(n[i]-1):
-        A[j+1, j] = a[j]
-        A[j, j+1] = a[j]
+        h = 1/(n[i]+1)
+        x = np.linspace(0, 1, n[i]+2)
+        f = h**2*100*np.exp(-10*x)
+        u_sol = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
 
-    start = time.process_time()
-    LU, P = scipy.linalg.lu_factor(A)
+        u_lu = np.zeros(n[i]+2)
 
-    h = 1/(n[i]+1)
-    x = np.linspace(0, 1, n[i]+2)
-    f = h**2*100*np.exp(-10*x)
-    u_sol = 1 - (1-np.exp(-10))*x - np.exp(-10*x)
+        for k in range(n[i]-1):
+            A[k+1, k] = a[k]
+            A[k, k+1] = a[k]
 
-    u_lu = np.zeros(n[i]+2)
-    u_lu[1:-1] = scipy.linalg.lu_solve((LU, P), f[1:-1])
-    end = time.process_time()
-    t[i] = end - start
+        start = time.process_time()
+        LU, P = scipy.linalg.lu_factor(A)
+        u_lu[1:-1] = scipy.linalg.lu_solve((LU, P), f[1:-1])
+        end = time.process_time()
+
+        t[j] = end - start
+    times_LU_decomp[i, 0] = t.mean()
+    times_LU_decomp[i, 1] = t.std()
 
     plt.plot(x, u_lu, label="LU-decomp")
     plt.plot(x, u_sol, label="Closed-form")
@@ -37,5 +43,13 @@ for i in range(len(n)):
     plt.legend()
     plt.show()
 
-print(n)
-print(t)
+plt.errorbar(n, times_LU_decomp[:,0], yerr = times_LU_decomp[:,1], c='red',
+             fmt="none", capsize=5)
+plt.scatter(n, times_LU_decomp[:,0], c='red', s=10)
+plt.xscale("log")
+plt.yscale("log")
+plt.title("Algorithm run times - LU decomposition")
+plt.xlabel('log $n$')
+plt.ylabel("log time [s]")
+plt.savefig("Figures/time_plot_LU.png")
+plt.show()
