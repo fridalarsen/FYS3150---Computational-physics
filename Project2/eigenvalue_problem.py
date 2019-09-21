478a1e74f73bedbc31c10f2dd5e8d85c88ed01db
Frida Larsen <friidamor@gmail.com>
2019-09-21 17:12:31 +0200

    Added function for finding eigenvalues using NumPy.

--- Project2/eigenvalue_problem.py
+++ Project2/eigenvalue_problem.py
@@ -4,26 +4,39 @@ class EigenvalueProblem:
     """
     Class for finding the eigenvalues of a matrix.
     """
-
-    def __init__(self, A):
+    def __call__(self, A):
         """
         Arguments:
-            A (matrix): Symmetric matrix whose eigenvalues are to be found.
-        """
-        self.A = A
-        self.N = len(A[0])
+            A (matrix): Matrix whose eigenvalues are to be determined.
+            method (str): Method to be used for finding eigenvalues.
+                          Options: NumPy
 
-    def NumpyEigenvalues(self):
+        Returns:
+            eig (array): Eigenvalues of A determined by specified method.
+        """
+        methods = ["NumPy"]
+        if method not in methods:
+            raise ValueError(f"Please choose one of the following methods:\
+                             {methods}")
+
+        if method == "NumPy":
+            return self.NumpyEigenvalues(A)
+
+
+    def NumpyEigenvalues(self, A):
         """
         Function implementing NumPy's functions for diagonalizing a matrix and
         finding eigenvalues.
 
         Returns:
-            EigenValues (array): List of eigenvalues.
+            EigenValues (array): List of eigenvalues sorted from smallest to
+                                 largest.
         """
-        EigenValues, EigenVectors = np.linalg.eig(self.A)
+        EigenValues, EigenVectors = np.linalg.eig(A)
 
-        return EigenValues 
+        EigenValues = EigenValues[np.argsort(EigenValues)]
+
+        return EigenValues
 
 
 
