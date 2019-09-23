b1e1e66db4f31ba4ac83c7ba00af4925a574e322
Frida Larsen <friidamor@gmail.com>
2019-09-23 14:39:36 +0200

    Added tests for non-diagonal symmetric matrices for numpy and jacobi methods.

--- Project2/test_eigenvalue_problem.py
+++ Project2/test_eigenvalue_problem.py
@@ -1,7 +1,7 @@
 import numpy as np
 from eigenvalue_problem import EigenvalueProblem
 
-def test_NumpyEigenvalues():
+def test_NumpyEigenvalues_diagonal():
     """
     Function for testing that NumpyEigenvalues returns the analytic eigenvalues
     for a diagonal matrix.
@@ -17,7 +17,7 @@ def test_NumpyEigenvalues():
     assert abs(calc_eig[1] - exp_eig[1]) < tol
     assert abs(calc_eig[2] - exp_eig[2]) < tol
 
-def test_JacobiEigenvalues():
+def test_JacobiEigenvalues_diagonal():
     """
     Function for testing that JacobiEigenvalues returns the analytic eigenvalues
     for a diagonal matrix.
@@ -28,6 +28,39 @@ def test_JacobiEigenvalues():
     calc_eig = Eig(A, "Jacobi")
     exp_eig = [1, 2, 3]
     tol = 1e-5
+
+    assert abs(calc_eig[0] - exp_eig[0]) < tol
+    assert abs(calc_eig[1] - exp_eig[1]) < tol
+    assert abs(calc_eig[2] - exp_eig[2]) < tol
+
+def test_NumpyEigenvalues():
+    """
+    Function for testing that NumpyEigenvalues returns the analytic eigenvalues
+    for a non-diagonal matrix.
+    """
+    A = np.array([[6, -2, -1], [-2, 6, -1], [-1, -1, 5]])
+    Eig = EigenvalueProblem()
+
+    calc_eig = Eig(A, "NumPy")
+    exp_eig = [3, 6, 8]
+    tol = 1e-5
+
+    assert abs(calc_eig[0] - exp_eig[0]) < tol
+    assert abs(calc_eig[1] - exp_eig[1]) < tol
+    assert abs(calc_eig[2] - exp_eig[2]) < tol
+
+def test_JacobiEigenvalues():
+    """
+    Function for testing that JacobiEigenvalues returns the analytic eigenvalues
+    for a non-diagonal symmetric matrix.
+    """
+    A = np.array([[6, -2, -1], [-2, 6, -1], [-1, -1, 5]])
+    Eig = EigenvalueProblem()
+
+    calc_eig = Eig(A, "Jacobi")
+    exp_eig = [3, 6, 8]
+    tol = 1e-5
+    print(calc_eig)
 
     assert abs(calc_eig[0] - exp_eig[0]) < tol
     assert abs(calc_eig[1] - exp_eig[1]) < tol
