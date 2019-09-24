23417c6dd4cc321783021a90dd36cefdd601c757
Frida Larsen <friidamor@gmail.com>
2019-09-24 13:50:58 +0200

    Added test for preserved orthogonality in Jacobi method.

--- Project2/test_eigenvalue_problem.py
+++ Project2/test_eigenvalue_problem.py
@@ -9,7 +9,7 @@ def test_NumpyEigenvalues_diagonal():
     A = np.diag([1, 2, 3])
     Eig = EigenvalueProblem()
 
-    calc_eig = Eig(A, "NumPy")
+    calc_eig, vec = Eig(A, "NumPy")
     exp_eig = [1, 2, 3]
     tol = 1e-5
 
@@ -25,7 +25,7 @@ def test_JacobiEigenvalues_diagonal():
     A = np.diag([1, 2, 3])
     Eig = EigenvalueProblem()
 
-    calc_eig = Eig(A, "Jacobi")
+    calc_eig, vec = Eig(A, "Jacobi")
     exp_eig = [1, 2, 3]
     tol = 1e-5
 
@@ -42,7 +42,7 @@ def test_NumpyEigenvalues():
                  [9,9,9,9,-21]])
     Eig = EigenvalueProblem()
 
-    calc_eig = Eig(A, "NumPy")
+    calc_eig, vec = Eig(A, "NumPy")
     exp_eig = [-30., 6., 6., 15., 30.]
     tol = 1e-5
 
@@ -61,7 +61,7 @@ def test_JacobiEigenvalues():
                  [9,9,9,9,-21]])
     Eig = EigenvalueProblem()
 
-    calc_eig = Eig(A, "Jacobi", 300)
+    calc_eig, vec = Eig(A, "Jacobi", 300)
     exp_eig = [-30., 6., 6., 15., 30.]
     tol = 1e-5
 
@@ -70,3 +70,25 @@ def test_JacobiEigenvalues():
     assert abs(calc_eig[2] - exp_eig[2]) < tol
     assert abs(calc_eig[3] - exp_eig[3]) < tol
     assert abs(calc_eig[4] - exp_eig[4]) < tol
+
+def test_JacobiEigenvalues_orthogonality():
+    """
+    Function for testing that JacobiEigenvalues preserves orthogonality after
+    performing the similarity transform.
+    """
+    A = np.array([[8,2,2,-6,9], [2,8,2,-6,9],[2,2,8,-6,9],[-6,-6,-6,24,9],\
+                 [9,9,9,9,-21]])
+    N = A.shape[0]
+    tol = 1e-5
+
+    Eig = EigenvalueProblem()
+
+    calc_eig, calc_vec = Eig(A, "Jacobi")
+
+    for i in range(N):
+        for j in range(N):
+            if i == j:
+                assert abs(np.dot(calc_vec[i], calc_vec[j]) - 1) < tol
+            if i != j:
+                assert abs(np.dot(calc_vec[i], calc_vec[j])) < tol
+                
