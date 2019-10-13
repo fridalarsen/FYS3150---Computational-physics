733e7cd8406fda64be8626eb2af29054e23c3be7
Frida Larsen <friidamor@gmail.com>
2019-10-13 20:48:28 +0200

    Added function for finding zeros and weights with Legendre polynomials.

--- Project3/num_int.cpp
+++ Project3/num_int.cpp
@@ -27,9 +27,60 @@ double Legendre(int n, double x)
 }
 
 
-void GaussLegendre()
+extern "C" void GaussLegendre(double a, double b, double **x, double **w, int N,
+                              double tol)
+/* Function for finding zeros and weights using Legendre polynomials in the
+   Gaussian quadrature.
+
+   Arguments:
+      a (double): lower integration limit
+      b (double): upper integration limit
+      x (double **): empty 1d column array, length N
+      w (double **): empty 1d column array, length N
+      N (int): number of integration points
+
+   "Returns":
+      x (double **): filled with zeros of Legendre polynomial of Nth degree.
+      w (double **): filled with integration weights.
+*/
 {
-  int N;
+  double pi = 3.14159265359;
+  int m = (N + 1)/2;
+  double guess;
+  double next_guess;
+  double leg, dleg;
+
+  // transform integration limits
+  double center = (b+a) / 2;
+  double radius = (b-a) / 2;
+
+  // for loop for finding zeros of L_N
+  for(int i=0; i <= m; i++){
+    next_guess = cos(pi * ((i+1)-0.25)/(N+0.5));  // initial guess of zero x-value
+    guess = next_guess + 2*tol;               // dummy value to enter while-loop
+
+    // Newton's method
+    while(fabs(guess - next_guess) > tol){
+      guess = next_guess;
+
+      leg = Legendre(N, guess);          // current Legendre polynomial value
+      dleg = N*(guess * leg - Legendre(N-1, guess)) / (guess*guess - 1); // derivative
+
+      next_guess = guess - (leg/dleg);
+    }
+    
+    // set x-values
+    x[i][0]   = center - radius*next_guess;
+    x[N-1-i][0] = center + radius*next_guess;   // using the fact that the roots are symmetric
+
+    // set w-values
+    w[i][0]   = 2*center / ((1 - next_guess*next_guess) * dleg*dleg);
+    w[N-1-i][0] = w[i][0];
+
+  }
+
+
+
 
 
 }
