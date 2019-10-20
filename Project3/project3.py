2672dcb3404bed86f9b0d3b7b2b3aedb8971215a
Frida Larsen <friidamor@gmail.com>
2019-10-20 14:43:18 +0200

    Added Monte Carlo code.

--- Project3/project3.py
+++ Project3/project3.py
@@ -15,6 +15,7 @@ class Project3:
         """
         os.system("g++ -std=c++11 -o project3_.o -c -O3 project3_.cpp")
         os.system("g++ -shared -fPIC -o project3_.so project3_.o")
+        os.system("mpicxx MC_parallelize.cpp -o MC_parallelize.x")
 
         cpp_lib = ctypes.cdll.LoadLibrary(os.path.abspath("./project3_.so"))
 
@@ -25,6 +26,17 @@ class Project3:
         self.solve3b = cpp_lib.Solve3b
         self.solve3b.restype = ctypes.c_double
         self.solve3b.argtypes = [ctypes.c_int, ctypes.c_double]
+
+        self.solve3c = cpp_lib.Solve3c
+        self.solve3c.restype = None
+        self.solve3c.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_double),
+                                 ctypes.POINTER(ctypes.c_double),
+                                 ctypes.c_double, ctypes.c_double]
+
+        self.solve3d = cpp_lib.Solve3d
+        self.solve3d.restype = None
+        self.solve3d.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_double),
+                                 ctypes.POINTER(ctypes.c_double), ctypes.c_int]
 
     def __call__(self, task, **kwargs):
         """
@@ -37,21 +49,37 @@ class Project3:
         Returns:
             I (float): Computed integral value.
             t (float): Time taken to compute integral.
+
+            var (float): Variance of computed integral. (Only returned for the
+                         Monte Carlo methods)
         """
-        tasks = ["3a", "3b"]
+        tasks = ["3a", "3b", "3c", "3d", "3e"]
         if task not in tasks:
-            raise ValueError(f"Task {task} is invalid. Options: {tasks}")
+            raise ValueError(f"Task not correctly chosen. Options: {tasks}")
 
         if task == "3a":
             solve3X = self.run_solve3a
         elif task == "3b":
             solve3X = self.run_solve3b
+        elif task == "3c":
+            solve3X = self.run_solve3c
+        elif task == "3d":
+            solve3X = self.run_solve3d
+        elif task == "3e":
+            solve3X = self.run_solve3e
 
-        start = time.perf_counter()
-        I = solve3X(**kwargs)
-        end = time.perf_counter()
-        t = end-start
-        return I, t
+        if task == "3a" or task == "3b":
+            start = time.perf_counter()
+            I = solve3X(**kwargs)
+            end = time.perf_counter()
+            t = end-start
+            return I, t
+        else:
+            start = time.perf_counter()
+            I, var = solve3X(**kwargs)
+            end = time.perf_counter()
+            t = end-start
+            return I, var, t
 
     def run_solve3a(self, lamb, N, tol=1e-8):
         """
@@ -84,12 +112,86 @@ class Project3:
         I = self.solve3b(int(N), float(tol))
         return I
 
+    def run_solve3c(self, N, a, b):
+        """
+        Function for computing the integral with brute force Monte Carlo.
+
+        Argumetns:
+            N (int): Number of integration points.
+            a (float): Lower integration limit.
+            b (float): Upper integration limit.
+
+        Returns:
+            I (float): Computed integral value.
+            var (float): Variance of computed integral.
+        """
+        I = ctypes.c_double()
+        var = ctypes.c_double()
+
+        self.solve3c(int(N), ctypes.byref(I), ctypes.byref(var), float(a),
+                     float(b))
+        I = I.value
+        var = var.value
+        return I, var
+
+    def run_solve3d(self, N, seed=None):
+        """
+        Function for computing the integral with Monte Carlo using importance
+        sampling and spherical coordinates.
+
+        Argumetns:
+            N (int): Number of integration points.
+            seed (int, optional): Seed for random number generator.
+
+        Returns:
+            I (float): Computed integral value.
+            var (float): Variance of computed integral.
+        """
+        if seed == None:
+            seed = np.random.randint(100)
+
+        I = ctypes.c_double()
+        var = ctypes.c_double()
+
+        self.solve3d(int(N), ctypes.byref(I), ctypes.byref(var), int(seed))
+        I = I.value
+        var = var.value
+        return I, var
+
+    def run_solve3e(self, N, npar):
+        """
+        Function for computing the integral with Monte Carlo using importance
+        sampling and spherical coordinates.
+
+        Argumetns:
+            N (int): Number of integration points.
+            npar (int): Number of parallel processes.
+
+        Returns:
+            I (float): Computed integral value.
+            var (float): Variance of computed integral.
+        """
+        if os.path.exists("results_3e.txt"):
+            os.remove("results_3e.txt")
+
+        os.system(f"mpiexec -n {npar} ./MC_parallelize.x {N}")
+
+        with open("results_3e.txt", "r") as results:
+            I, var = [float(line) for line in results.readlines()]
+
+        return I, var
 
 if __name__ == "__main__":
     P3 = Project3()
     I_a, t_a = P3("3a", lamb=2.2, N=15, tol=1e-8)
     I_b, t_b = P3("3b", N=15, tol=1e-8)
+    I_c, var_c, t_c = P3("3c", N=500000, a=-2.2, b=2.2)
+    I_d, var_d, t_d = P3("3d", N=200000)
+    I_e, var_e, t_e = P3("3e", N=100000, npar=2)
 
     print(f"exact = {5*(np.pi**2)/(16**2)}")
     print(f"3a    = {I_a}")
     print(f"3b    = {I_b}")
+    print(f"3c    = {I_c}")
+    print(f"3d    = {I_d}")
+    print(f"3e    = {I_e}")
--- Project3/project3_.cpp
+++ Project3/project3_.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include "gauss-laguerre.cpp"
+#include <random>
 
 using namespace std;
 
@@ -39,6 +40,7 @@ void GaussLegendre(double a, double b, double *x, double *w, int N,
       x (double *): empty 1d column array, length N.
       w (double *): empty 1d column array, length N.
       N (int): number of integration points.
+      tol (double): Tolerance for Newton's method.
 
    "Returns":
       x (double *): filled with zeros of Legendre polynomial of Nth degree.
@@ -81,7 +83,7 @@ void GaussLegendre(double a, double b, double *x, double *w, int N,
 }
 
 double f(double x1, double y1, double z1, double x2, double y2, double z2,
-         double alpha=2, double tol = 1e-10)
+         double alpha=2)
 {
     double r1 = sqrt(x1*x1 + y1*y1 + z1*z1);
     double r2 = sqrt(x2*x2 + y2*y2 + z2*z2);
@@ -89,6 +91,7 @@ double f(double x1, double y1, double z1, double x2, double y2, double z2,
     double num = exp(-2*alpha*(r1+r2));
     double denom = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
 
+    double tol = 1e-8;
     if(denom < tol){
       return 0;
     }else{
@@ -97,7 +100,7 @@ double f(double x1, double y1, double z1, double x2, double y2, double z2,
 }
 
 double g(double r1, double r2, double theta1, double theta2, double phi1,
-         double phi2, double tol)
+         double phi2)
 {
   double sin1 = sin(theta1);
   double sin2 = sin(theta2);
@@ -109,6 +112,7 @@ double g(double r1, double r2, double theta1, double theta2, double phi1,
 
   double r12r12 = r1*r1 + r2*r2 - 2*r1*r2*cos_beta;
 
+  double tol = 1e-8;
   if (r12r12 < tol){
     return 0;
   }else{
@@ -117,12 +121,12 @@ double g(double r1, double r2, double theta1, double theta2, double phi1,
 }
 
 double EstimateCartesianInt(double *x, double *w, int N)
-/* Function for computing a six-dimensional integral numerically using Gauss-
-   Legendre quadrature.
+/* Function for computing the six-dimensional integral using Gauss-Legendre
+   quadrature.
 
    Arguments:
-      x (double *): empty 1d column array, length N.
-      w (double *): empty 1d column array, length N.
+      x (double *): zeros of Legendre function.
+      w (double *): weights corresponding to zeros of Legendre function.
       N (int): number of integration points.
 
    Returns:
@@ -151,14 +155,29 @@ double EstimateCartesianInt(double *x, double *w, int N)
     for (int n=0; n<N; n++){
       z2 = x[n];
       w6 = w5*w[n];
-      I += w6*f(x1,y1,z1,x2,y2,z2);
+      I += w6*f(x1, y1, z1, x2, y2, z2);
     }}}}}}
     return I;
 }
 
 double EstimateSphericalInt(double *r, double *w_r, double *theta,
-                            double *w_theta, double *phi, double *w_phi, int N,
-                            double tol)
+                            double *w_theta, double *phi, double *w_phi, int N)
+/* Function for computing the six-dimensional integral using Gauss-Legendre
+   quadrature in spherical coordinates.
+
+   Arguments:
+      r (double *): Radial component of Laguerre zeros.
+      w_r (double *): Weights corresponding to radial component of Laguerre
+                      zeros.
+      theta (double *): Angular component (0,pi) of Legendre zeros.
+      w_theta (double *): Weights corresponding to theta.
+      phi (double *): Angular component (0,2pi) of Legendre zeros.
+      w_phi (double *): Weights corresponding to phi.
+      N (int): number of integration points.
+
+   Returns:
+      I (float): estimated value of integral.
+*/
 {
     double I = 0.;
     double r1, r2, theta1, theta2, phi1, phi2;
@@ -182,12 +201,22 @@ double EstimateSphericalInt(double *r, double *w_r, double *theta,
     for (int n=0; n<N; n++){
       phi2 = phi[n];
       w6 = w5*w_phi[n];
-      I += w6*g(r1, r2, theta1, theta2, phi1, phi2, tol);
+      I += w6*g(r1, r2, theta1, theta2, phi1, phi2);
     }}}}}}
     return I;
 }
 
 extern "C" double Solve3a(double lambda, int N, double tol)
+/* Function for solving problem 3a using Legendre polynomials.
+
+   Arguments:
+    lambda (double): Limit(s) of integration.
+    N (int): Number of integration points.
+    tol (double): Tolerance of Newton's method in the GaussLegendre function.
+
+   Returns:
+    I (double): Computed value of integral.
+*/
 {
   double *x = new double [N];
   double *w = new double [N];
@@ -203,6 +232,15 @@ extern "C" double Solve3a(double lambda, int N, double tol)
 }
 
 extern "C" double Solve3b(int N, double tol)
+/* Function for solving problem 3b using Legendre and Laguerre polynomials.
+
+   Arguments:
+    N (int): Number of integration points.
+    tol (double): Tolerance of Newton's method in the GaussLegendre function.
+
+   Returns:
+    I (double): Computed value of integral.
+*/
 {
   double pi = 3.14159265359;
   double alf = 2.0;
@@ -220,7 +258,7 @@ extern "C" double Solve3b(int N, double tol)
 
   gauss_laguerre(r, w_r, N, alf);
 
-  double I = EstimateSphericalInt(r, w_r, theta, w_theta, phi, w_phi, N, tol);
+  double I = EstimateSphericalInt(r, w_r, theta, w_theta, phi, w_phi, N);
 
   delete[] theta;
   delete[] phi;
@@ -230,4 +268,128 @@ extern "C" double Solve3b(int N, double tol)
   delete[] w_r;
 
   return I;
+}
+
+extern "C" void Solve3c(int N, double & I, double & var, double a,
+                                double b)
+/* Function for computing an integral numerically using a crude Monte Carlo
+   method.
+
+   Arguments:
+    N (int): Number of Monte Carlo samples.
+    I (double): Double to hold integral value.
+    var (double): Double to hold variance.
+    a (double): Lower integration limit.
+    b (double): Upper integration limit.
+
+   "Returns":
+    I (double): Computed integral value.
+    var (double): Variance of computed integral value.
+*/
+{
+  mt19937_64 generator;
+  uniform_real_distribution<double> distribution(a,b);
+
+  double x1, y1, z1, x2, y2, z2;
+  double y;
+  double z = 0;
+  double I_temp = 0;
+  double var_temp = 0;
+  double p = 1/pow((b-a), 6);
+
+  for (int i=0; i<N; i++){
+    x1 = distribution(generator);
+    y1 = distribution(generator);
+    z1 = distribution(generator);
+    x2 = distribution(generator);
+    y2 = distribution(generator);
+    z2 = distribution(generator);
+
+    y = f(x1, y1, z1, x2, y2, z2)/p;
+    I_temp += y;
+    z += y*y;
+  }
+
+  I_temp = I_temp / ((double) N);
+  z = z / ((double) N);
+  var_temp = z - I_temp*I_temp;
+
+  I = I_temp;
+  var = var_temp;
+}
+
+double h(double r1, double r2, double theta1, double theta2, double phi1,
+         double phi2)
+{
+  double sin1 = sin(theta1);
+  double sin2 = sin(theta2);
+  double cos1 = cos(theta1);
+  double cos2 = cos(theta2);
+
+  double cos_diff = cos(phi1 - phi2);
+  double cos_beta = cos1*cos2 + sin1*sin2*cos_diff;
+
+  double r12r12 = r1*r1 + r2*r2 - 2*r1*r2*cos_beta;
+
+  double tol = 1e-8;
+  if (r12r12 < tol){
+    return 0;
+  }else{
+    return sin1*sin2*(r1*r1*r2*r2)*exp(-4*(r1+r2))/sqrt(r12r12);
+  }
+}
+
+extern "C" void Solve3d(int N, double & I, double & var, int seed)
+/* Function for computing an integral numerically using Monte Carlo with
+   importance sampling and spherical coordinates.
+
+   Arguments:
+    N (int): Number of Monte Carlo samples.
+    I (double): Double to hold integral value.
+    var (double): Double to hold variance value.
+    seed (int): Seed for random number generator.
+
+   "Returns":
+    I (double): Computed integral value.
+    var (double): Variance of computed integral value.
+*/
+{
+  double pi = 3.14159265359;
+
+  mt19937_64 generator(seed);
+  uniform_real_distribution<double> distribution(0, 1);
+
+  double x1, x2;
+  double r1, r2, theta1, theta2, phi1, phi2;
+  double y;
+  double z = 0;
+  double I_temp = 0;
+  double var_temp = 0;
+
+  double p_ = 1/(4*pow(pi, 4)) ;
+  for (int i=0; i<N; i++){
+    x1 = distribution(generator);
+    x2 = distribution(generator);
+
+    r1 = -log(1-x1);
+    r2 = -log(1-x2);
+
+    theta1 = pi*distribution(generator);
+    theta2 = pi*distribution(generator);
+    phi1 = 2*pi*distribution(generator);
+    phi2 = 2*pi*distribution(generator);
+
+    double p = p_*exp(-(r1+r2));
+    y = h(r1, r2, theta1, theta2, phi1, phi2)/p;
+    I_temp += y;
+    z += y*y;
+
+  }
+
+  I_temp = I_temp / ((double) N);
+  z = z / ((double) N);
+  var_temp = z - I_temp*I_temp;
+
+  I = I_temp;
+  var = var_temp;
 }
